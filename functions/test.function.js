e1258cb23283937ce9dad9834cae2f73c3eacd46
libsys <libsys@localhost.localdomain>
2019-08-21 14:55:40 +0530

    test 6

--- functions/test.function.js
+++ functions/test.function.js
@@ -10,4 +10,19 @@
     
      agent.add(`I didn't understand`);
      agent.add(`I'm sorry, can you try again?`);
+         agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+    agent.add(new Card({
+        title: `Title: this is a card title`,
+        imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+        text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+        buttonText: 'This is a button',
+        buttonUrl: 'https://assistant.google.com/'
+      })
+    );
+    agent.add(new Suggestion(`Quick Reply`));
+    agent.add(new Suggestion(`Suggestion`));
+    agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+    let conv = agent.conv(); // Get Actions on Google library conv instance
+    conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+    agent.add(conv); // Add Actions on Google library responses to your agent's response
    };
