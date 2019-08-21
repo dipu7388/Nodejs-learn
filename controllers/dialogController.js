7140650dc72ef13056f9020f085709f7f570c036
libsys <libsys@localhost.localdomain>
2019-08-21 16:47:46 +0530

    test 9

--- controllers/dialogController.js
+++ controllers/dialogController.js
@@ -30,8 +30,98 @@ function dialogController(Dialog) {
       // intentMap.set('your intent name here', googleAssistantHandler);
       agent.handleRequest(intentMap);
     } catch (error) {
-      console.log(testAgent,"TEst Agent");
-      response.send(buildChatResponse(`I'm sorry, I don't know this ${error}`));
+
+      let req={
+   
+    "responseId": "336069a5-2b5a-4ab2-b4cf-972781107632-5e314962",
+    "queryResult": {
+      "queryText": "test",
+      "parameters": {},
+      "allRequiredParamsPresent": true,
+      "fulfillmentMessages": [
+        {
+          "text": {
+            "text": [
+              "I didn't understand"
+            ]
+          }
+        },
+        {
+          "text": {
+            "text": [
+              "I'm sorry, can you try again?"
+            ]
+          }
+        },
+        {
+          "text": {
+            "text": [
+              "This message is from Dialogflow's Cloud Functions for Firebase editor!"
+            ]
+          }
+        },
+        {
+          "card": {
+            "title": "Title: this is a card title",
+            "subtitle": "This is the body text of a card.  You can even use line\n  breaks and emoji! 💁",
+            "imageUri": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+            "buttons": [
+              {
+                "text": "This is a button",
+                "postback": "https://assistant.google.com/"
+              }
+            ]
+          }
+        },
+        {
+          "quickReplies": {
+            "quickReplies": [
+              "Quick Reply",
+              "Suggestion"
+            ]
+          }
+        }
+      ],
+      "outputContexts": [
+        {
+          "name": "projects/lsnetx-chatbot-pspxhh/agent/sessions/55d64aed-c682-088a-f676-a7004cfc3e48/contexts/weather",
+          "lifespanCount": 2,
+          "parameters": {
+            "city": "Rome"
+          }
+        }
+      ],
+      "intent": {
+        "name": "projects/lsnetx-chatbot-pspxhh/agent/intents/994db840-d37c-416c-b8b7-1963b859a296",
+        "displayName": "test"
+      },
+      "intentDetectionConfidence": 1,
+      "diagnosticInfo": {
+        "webhook_latency_ms": 52
+      },
+      "languageCode": "en"
+    },
+    "webhookStatus": {
+      "message": "Webhook execution successful"
+    },
+    "session": "projects/lsnetx-chatbot-pspxhh/agent/sessions/55d64aed-c682-088a-f676-a7004cfc3e48"
+   };
+   try {
+    request.body=req;
+    let x= request;
+    const agent1 = new WebhookClient({
+      request : x,
+      response : response
+    });
+        let intentMap = new Map();
+        intentMap.set('test', testAgent);
+        intentMap.set('lsnetx.contact-us', contactUs);
+        // intentMap.set('your intent name here', googleAssistantHandler);
+        agent1.handleRequest(intentMap);
+      } catch (error) {
+        response.send(buildChatResponse(`I'm sorry, I don't know this ${error}`));
+      }
+
     }
   }
 
