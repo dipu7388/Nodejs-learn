caf3ea20668f9ab5395990151a97d9cf76c4ecf9
libsys <libsys@localhost.localdomain>
2019-08-28 18:28:45 +0530

    Dialogflow event fallback

--- functions/initialize.js
+++ functions/initialize.js
@@ -9,5 +9,4 @@ module.exports = function (agent) {
   agent.add(new Suggestion(`Would like to know about your services?`));
   agent.add(new Suggestion(`What Retail Software you Provide?`));
   agent.add(new Suggestion(`Do you make website for retailers?`));
-  agent.add(new Suggestion(`Other`));
 };
